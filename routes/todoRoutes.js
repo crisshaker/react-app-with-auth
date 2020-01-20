490cf73d722a3633cd0426dd3365327a5e3e8b83
Christian Owusu <crisshaker.dev@gmail.com>
2020-01-20 20:41:45 +0000

    added fetchTodo middleware

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,6 +2,31 @@ const mongoose = require('mongoose');
 const Todo = mongoose.model('Todo');
 const requireAuth = require('../middlewares/requireAuth');
 
+async function fetchTodo(req, res, next) {
+  const { todoID } = req.params;
+
+  if (!todoID) {
+    return res.status(400).send({ error: 'bad request' });
+  }
+
+  try {
+    const todo = await Todo.findById(todoID);
+    if (!todo) {
+      return res.status(404).send({ error: 'todo not found' });
+    }
+
+    if (req.user._id.toString() !== todo._user.toString()) {
+      return res.status(403).send({ error: 'authorization required' });
+    }
+
+    req.todo = todo;
+    next();
+  } catch (err) {
+    res.status(500).send({ error: 'request failed' });
+    console.log(err);
+  }
+}
+
 async function getTodos(req, res) {
   try {
     const todos = await Todo.find({ _user: req.user._id });
@@ -26,23 +51,8 @@ async function createTodo(req, res) {
 }
 
 async function deleteTodo(req, res) {
-  const { todoID } = req.params;
-
-  if (!todoID) {
-    return res.status(400).send({ error: 'bad request' });
-  }
-
   try {
-    const todo = await Todo.findById(todoID);
-    if (!todo) {
-      return res.status(404).send({ error: 'todo not found' });
-    }
-
-    if (req.user._id.toString() !== todo._user.toString()) {
-      return res.status(403).send({ error: 'authorization required' });
-    }
-
-    await todo.remove();
+    await req.todo.remove();
     res.send({ success: true });
   } catch (err) {
     res.status(500).send({ error: 'request failed' });
@@ -51,23 +61,8 @@ async function deleteTodo(req, res) {
 }
 
 async function getTodo(req, res) {
-  const { todoID } = req.params;
-
-  if (!todoID) {
-    return res.status(400).send({ error: 'bad request' });
-  }
-
   try {
-    const todo = await Todo.findById(todoID);
-    if (!todo) {
-      return res.status(404).send({ error: 'todo not found' });
-    }
-
-    if (req.user._id.toString() !== todo._user.toString()) {
-      return res.status(403).send({ error: 'authorization required' });
-    }
-
-    res.send(todo);
+    res.send(req.todo);
   } catch (err) {
     res.status(500).send({ error: 'request failed' });
     console.log(err);
@@ -75,29 +70,14 @@ async function getTodo(req, res) {
 }
 
 async function editTodo(req, res) {
-  const { todoID } = req.params;
-
-  if (!todoID) {
-    return res.status(400).send({ error: 'bad request' });
-  }
-
   const { title } = req.body;
   if (!(title && title.toString().trim())) {
     return res.status(403).send({ error: 'title required' });
   }
 
   try {
-    const todo = await Todo.findById(todoID);
-    if (!todo) {
-      return res.status(404).send({ error: 'todo not found' });
-    }
-
-    if (req.user._id.toString() !== todo._user.toString()) {
-      return res.status(403).send({ error: 'authorization required' });
-    }
-
-    todo.title = title;
-    await todo.save();
+    req.todo.title = title;
+    await req.todo.save();
     res.send({ success: true });
   } catch (err) {
     res.status(500).send({ error: 'request failed' });
@@ -107,8 +87,8 @@ async function editTodo(req, res) {
 
 module.exports = app => {
   app.post('/todos', requireAuth, createTodo);
-  app.get('/todos/:todoID', requireAuth, getTodo);
+  app.get('/todos/:todoID', requireAuth, fetchTodo, getTodo);
   app.get('/todos', requireAuth, getTodos);
-  app.patch('/todos/:todoID', requireAuth, editTodo);
-  app.delete('/todos/:todoID', requireAuth, deleteTodo);
+  app.patch('/todos/:todoID', requireAuth, fetchTodo, editTodo);
+  app.delete('/todos/:todoID', requireAuth, fetchTodo, deleteTodo);
 };
